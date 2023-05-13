51e2cf0b09afb25f7a6f75b839915763a97e5271
TheLegend <amoghvarote@gmail.com>
2023-05-13 12:34:36 +0400

    k

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ document.addEventListener('DOMContentLoaded', function() {
     element.classList.add('show');
   });
 });
+
+const sections = document.querySelectorAll('.parallax');
+
+function parallaxScroll() {
+  sections.forEach(section => {
+    const scrollPosition = window.pageYOffset;
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionBottom = sectionTop + sectionHeight;
+    const sectionSpeed = section.dataset.speed || 0.5;
+    const translateValue = (scrollPosition - sectionTop) * sectionSpeed;
+    
+    if (scrollPosition + window.innerHeight > sectionTop && scrollPosition < sectionBottom) {
+      section.querySelector('.parallax__content').style.transform = `translate(-50%, calc(-50% + ${translateValue}px))`;
+    }
+  });
+}
+
+window.addEventListener('scroll', () => {
+  requestAnimationFrame(parallaxScroll);
+});
+
+window.addEventListener('load', () => {
+  parallaxScroll();
+});
