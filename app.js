6245467f633849a6e508f3c12916264ebf2edb33
TheLegend <amoghvarote@gmail.com>
2023-05-16 22:11:54 +0400

    it was surely 10 mins

--- app.js
+++ app.js
@@ -37,38 +37,23 @@ window.addEventListener('load', () => {
 
 const track = document.getElementById("image-track");
 
+
+window.onmouseup = () =>{
+  track.dataset.mouseDownAt = "0";
+}
+
 window.onmousedown = e =>{
   track.dataset.mouseDownAt = e.clientX;
 }
+
 
 window.onmousemove = e =>{
   if(track.dataset.mouseDownAt === "0") return;
 
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-
-        Math.min(nextPercentage, 0);
-        Math.max(nextPercentage, 100);
-
-  track.dataset.percentage = nextPercentage;
-
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-    }, { duration: 1200, fill: "forwards"});
+        maxDelta = window.innerWidth/2;
   
+  const percentage = (mouseDelta/maxDelta) * 100;
 
-
-window.onmouseup = () =>{
-  track.dataset.mouseDownAt = "0";
-  track.dataset.prevPercentage = track.dataset.percentage;
-}
-
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, {duration: 1200, fill: "forwards"});
-  }
+  track.style.transform = 'translate(${percentage}%, -50%)';
 }
