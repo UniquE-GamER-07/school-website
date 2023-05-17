6e67fa5bfc3e6d06ad1712d9f31876a40c2adada
TheLegend <amoghvarote@gmail.com>
2023-05-17 17:44:05 +0400

    LETS GO.

--- app.js
+++ app.js
@@ -35,25 +35,83 @@ window.addEventListener('load', () => {
   parallaxScroll();
 });
 
+// const track = document.getElementById("image-track");
+// console.log(track)
+
+// window.onmouseup = () =>{
+//   track.dataset.mouseDownAt = "0";
+// }
+
+// window.onmousedown = e =>{
+//   track.dataset.mouseDownAt = e.clientX;
+// }
+
+
+// window.addEventListener("mousemove", (e) => {
+//   if(track.dataset.mouseDownAt === "0") return;
+
+//   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+//         maxDelta = window.innerWidth/2;
+// console.log(track)
+        
+//   const percentage = (mouseDelta/maxDelta) * -100;
+
+//   track.style.transform = `translate(${percentage}%, -50%)`;
+// }
+// )
+
+// track.animate({
+//   transform: `translate(${nextPercentage}%, -50%)`
+// }, { duration: 1200, fill: "forwards" });
+
+// for(const image of track.getElementsByClassName("image")) {
+//   image.animate({
+//     objectPosition: `${100 + nextPercentage }% center`
+//   }, { duration: 1200, fill: "forwards" });
+// }
+
 const track = document.getElementById("image-track");
 
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-window.onmouseup = () =>{
-  track.dataset.mouseDownAt = "0";
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-window.onmousedown = e =>{
-  track.dataset.mouseDownAt = e.clientX;
-}
-
-
-window.onmousemove = e =>{
+const handleOnMove = e => {
   if(track.dataset.mouseDownAt === "0") return;
-
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth/2;
   
-  const percentage = (mouseDelta/maxDelta) * 100;
-
-  track.style.transform = 'translate(${percentage}%, -50%)';
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
