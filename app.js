21a3edf8f62eeb5ffbe45dc6693acdd5aa8ff59d
TheLegend <amoghvarote@gmail.com>
2023-06-20 21:13:35 +0400

    hell yea

--- app.js
+++ app.js
@@ -83,6 +83,53 @@ window.ontouchmove = e => handleOnMove(e.touches[0]);
 */
 
 const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+/*
+const track = document.getElementById("image-track");
 const body = document.querySelector("body");
 const nextPage = document.getElementById("page-2"); // Change 'page-2' to the ID of your second page element
 
@@ -131,7 +178,7 @@ const handleOnClick = () => {
   }
 };
 
-/* -- Had to add extra lines for touch events -- */
+
 
 window.onmousedown = e => handleOnDown(e);
 
@@ -146,7 +193,7 @@ window.onmousemove = e => handleOnMove(e);
 window.ontouchmove = e => handleOnMove(e.touches[0]);
 
 window.onclick = () => handleOnClick();
-
+*/
 
 /*function menuclick() {
   if document.getElementsByClassName('image') ==
